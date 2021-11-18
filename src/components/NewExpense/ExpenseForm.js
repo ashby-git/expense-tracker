7c059a6a7d2214792f8712bd087af7549f105979
Ashby McFall <ashbymcfall@hotmail.co.uk>
2021-11-18 14:04:13 +0900

    ✨add logic for year filter, add expense & its state

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -22,6 +22,7 @@ const ExpenseForm = (props) => {
 
     const submitHandler = (event) => {
         event.preventDefault();
+
         const expenseData = {
             title: enteredTitle,
             amount: enteredAmount,
@@ -66,6 +67,7 @@ const ExpenseForm = (props) => {
                 </div>
             </div>
             <div className="new-expense__actions">
+                <button type="button" onClick={props.onCancel}>Cancel</button>
                 <button type="submit">Add Expense</button>
             </div>
         </form>
